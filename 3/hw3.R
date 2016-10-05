b660642892a21cd4cf0d8eba0510019af555140d
Hannes Leskelä <hleskela@kth.se>
2016-10-05 12:15:34 +0200

    Working svm,training error 0.006608

--- 3/hw3.R
+++ 3/hw3.R
@@ -31,15 +31,86 @@
 # 8. Missing attribute values: none
 # 
 # 9. Class distribution: 357 benign, 212 malignant
-# If you haven't installed kernlab, uncomment and run
-install.packages('kernlab')
+# If you haven't installed kernlab, uncomment and run:
+#install.packages('kernlab')
 
 library(kernlab)
 
+set.seed(1991)
+
 # Import the data, split into two matrices
-complete_dataset <- read.csv("wdbc.data")
+setwd('/home/hannes/Documents/UPC/KBML/3/') # Change to wd where you keep the data
+complete_dataset <- read.csv("wdbc.data",stringsAsFactors=FALSE) #Try the saf=FALSE to swap M/B to 1/0
+N <- nrow(complete_dataset)
 diagnosis <- complete_dataset[,c(1,2)]
 #Stripping data of the id.s and M/B so we can normalize it
-normalized_data <- complete_dataset[,c(-1,-2)]
+normalizable_data <- complete_dataset[,c(-1,-2)]
+
+# Switch B/M to 0/1, then make numerical since they're seen as strings
+diagnosis[diagnosis=="B"] <- 0
+diagnosis[diagnosis=="M"] <- 1
+diagnosis[,2] <- strtoi(diagnosis[,2], base = 10)
+
 # Normalize the data
-scale(normalized_data)
+# Normalizing the data for SVMs is especially important depending on what kind of a distance function
+# one uses. If Euclidean distance is used, it is of utmost importance, since if one variable ranges between
+# 1-2 and another between 10-20, then the latter variable has a 10 to 20 times larger effect on the outcome
+# of the distance calculation compared to the first variable
+
+normalized_data <- scale(normalizable_data, center=FALSE, scale=colSums(normalizable_data))
+dataset <- cbind(diagnosis, normalized_data)
+
+
+## Now we define a utility function for performing k-fold CV
+## the learning data is split into k equal sized parts
+## every time, one part goes for validation and k-1 go for building the model (training)
+## the final error is the mean prediction error in the validation parts
+## Note k=N corresponds to LOOCV
+
+## a typical choice is k=10
+k <- 10 
+folds <- sample(rep(1:k, length=N), N, replace=FALSE) 
+
+valid.error <- rep(0,k)
+
+C <- 1
+i=1
+train <- dataset[folds!=i,] # for building the model (training)
+valid <- dataset[folds==i,] # for prediction (validation)
+
+x_train <- train[,1:2]
+t_train <- train[,3]
+
+model <- ksvm(as.matrix(x_train), t_train, type="C-svc", C=1, kernel='rbfdot',scaled=c())
+
+
+
+
+
+training.ratio <- 0.8
+ntrain <- round(N*training.ratio)
+training_part <- sample(N, ntrain)
+
+(train <- dataset[training_part,])
+(test <- dataset[-training_part,])
+
+x_train <- train[,c(3:32)]
+t_train <- train[,2]
+x_test <- test[,3:32]
+t_test <- test[,2]
+
+model <- ksvm(as.matrix(x_train), t_train, type="C-svc", C=1, kernel='rbfdot',scaled=c())
+
+model
+alpha(model)
+
+t_pred <- predict(model, x_test)
+
+table(t_test, t_pred)
+
+
+# Don't know, probably not right
+(VA.error.RBF <- train.svm.kCV ("RBF", C))
+
+model <- svm(dataset[,1:2],dataset[,3], type="C-classification", cost=C, kernel="radial", scale = FALSE)
+plot.prediction ("RBF")
