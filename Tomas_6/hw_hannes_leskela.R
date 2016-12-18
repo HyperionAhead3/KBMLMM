a18f72f7343b2807f7691d9ada2dd1888d6b6694
Hannes Leskelä <hleskela@kth.se>
2016-12-18 01:11:16 +0100

    Center data, do pls1.
    I don't think that I'm using data.frame correctly, nor the
    different function calls in between these conversions. But this
    pls1 model makes sense, unlike the first try. With this setup,
    the percent of the data explained by x components is:
    1: 78.16, 2: 91.20, 3:95.88, 4: 98.49, 5: 99.43
    This is reasonable results for training data.

--- Tomas_6/hw_hannes_leskela.R
+++ Tomas_6/hw_hannes_leskela.R
@@ -18,12 +18,31 @@ indep.response <- append(rep(1,20), rep(0,14) , after = 20)
 X <- t(train)
 Xt <- t(indep)
 
-# Remove non-numerical rows
+# Remove non-numerical rows and make data into data.frames for easier 
+# management and function calling.
 remove.from.train <- seq(4,78,2)
 X <- X[-remove.from.train,]
 X <- X[-c(1,2),]
+X <- data.frame(X)
 
 remove.from.indep <- seq(4,70,2)
 Xt <- Xt[-remove.from.indep,]
 Xt <- Xt[-c(1,2),]
+Xt <- data.frame(Xt)
 
+# Convert to numerical
+X <- data.frame(sapply(X, function(x) as.numeric(as.character(x))))
+Xt <- data.frame(sapply(Xt, function(x) as.numeric(as.character(x))))
+
+# scale X and Xt
+X_means = colMeans(X)
+X = scale(X, scale = FALSE)
+Xt = scale(Xt, center=X_means, scale = FALSE)
+
+X <- data.frame(X)
+Xt <- data.frame(Xt)
+
+p1 <- plsr(train.response ~ ., ncomp = 10, data = X[1:38,], validation = "LOO")
+
+attributes(p1)
+summary(p1)
